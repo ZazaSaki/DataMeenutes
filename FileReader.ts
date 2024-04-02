268a7a9d0ecbc88974b588234d9b5bcfebd1fc15
ZazaSaki <isac.cruzm@gmail.com>
2024-04-02 17:01:22 +0100

    Bug fix: not writning file

--- FileReader.ts
+++ FileReader.ts
@@ -1,13 +1,8 @@
-
 import * as fs from 'fs';
 
-const markdownText: string = fs.readFileSync('./subject.md', { encoding: 'utf-8' });
-
-
-
-
 export function readMDFile(Dir :string){
-    return fs.readFileSync(Dir,{encoding:'utf-8'});
+    let path : string = fs.realpathSync(Dir);
+    return fs.readFileSync(path,{encoding:'utf-8'});
 
 }
 
@@ -15,8 +10,5 @@ export function writeAnswer(Dir: string, outputName : string, data :string){
     Dir += `/${outputName}`;
 
     const path : string = fs.realpathSync(Dir);
-    fs.writeFileSync(Dir,"data", {encoding : "utf-8"})
+    fs.writeFileSync(Dir,data, {encoding : "utf-8"})
 }
-
-
-
